a6488c4fb7db362564bd2368f38fb06c2c88661a
Nafanja0k <sanja.nafanja@gmail.com>
2015-09-29 18:46:31 +0300

    opisanija grupp

--- src/FillProducts.java
+++ src/FillProducts.java
@@ -2,6 +2,7 @@
 import java.util.*;public class FillProducts
 {
 	static void fill(ArrayList<Product> products){
+		// Smotri Product.java dlia opisania parametrov
 		products.add(new Product("Яблоки",1,1,0));
 	}
 }
--- src/Product.java
+++ src/Product.java
@@ -6,6 +6,12 @@ public class Product
 	private int id;
 	private double price;
 	
+	// Gruppa 1 - Produkty;
+	// Podgruppa 1 - Frukty;
+	// Podgruppa 2 - Ofoschi;
+	// Gruppa 2 - Elektronika;
+	// Podgruppa 1 - Televizory;
+	// Podgruppa 2 - Telefony;
 	public void Product(String name, int group, int subGroup, int id){
 		this.name = name;
 		this.group = group;
