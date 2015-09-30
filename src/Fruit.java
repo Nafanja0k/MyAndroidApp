979acb12dddd52d49f26ce72a723f6db7e53f50c
Nafanja0k <sanja.nafanja@gmail.com>
2015-09-30 09:39:34 +0300

    Update Fruit.java

--- src/Fruit.java
+++ src/Fruit.java
@@ -1,8 +1,17 @@
 
 public class Fruit extends Food {
 
+	// націнка у зв’язку з усушкою/утряскою фруктів)
+	protected double shrinkage = 1,01;
+	// номер групи/класу. Використовується для генерації номера товару
+	protected classId=1;
+	
 	public Fruit() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public double getPrice(){
+		return this.inPrice * this.margin * this.shrinkage;
 	}
 
 }
