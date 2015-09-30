e7a3ade71d143c978edd832ec6dd0b36ff86aa5b
Nafanja0k <sanja.nafanja@gmail.com>
2015-09-30 09:40:44 +0300

    Update Vegetable.java

--- src/Vegetable.java
+++ src/Vegetable.java
@@ -1,8 +1,17 @@
 
 public class Vegetable extends Food {
 
-	public Vegetable() {
+	// націнка у зв’язку з усушкою/утряскою фруктів)
+	protected double shrinkage = 1,02;
+	// номер групи/класу. Використовується для генерації номера товару
+	protected classId=2;
+	
+	public Veegtable() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public double getPrice(){
+		return this.inPrice * this.margin * this.shrinkage;
 	}
 
 }
