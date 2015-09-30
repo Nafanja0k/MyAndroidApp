4fc95e8a7bb1ca4770033f6e200aa16de8c92b87
Nafanja0k <sanja.nafanja@gmail.com>
2015-09-30 09:30:39 +0300

    Update Product.java

--- src/Product.java
+++ src/Product.java
@@ -1,38 +1,14 @@
-public class Product
+abstract public class Product
 {
-	private String name;
-	private int group;
-	private int subGroup;
-	private int id;
-	private double price;
+	// Вхідна ціна товару
+	protected inPrice;
+	// назва товару
+	protected name;
 	
-	// Gruppa 1 - Produkty;
-	// Podgruppa 1 - Frukty;
-	// Podgruppa 2 - Ofoschi;
-	// Gruppa 2 - Elektronika;
-	// Podgruppa 1 - Televizory;
-	// Podgruppa 2 - Telefony;
-	public void Product(String name, int group, int subGroup, int id){
-		this.name = name;
-		this.group = group;
-		this.subGroup = subGroup;
-		this.id = id;
+	public void Product(String name, double inPrice){
+
 	};
 	
-	
-	public void setPrice(int product){
-		
-	
-	};
-	
-	public void addToCart(int product_id){
-		
-	}
-	
-	public void getProduct(int product_id){
-		
-		System.out.println(this.group + "." +this.subGroup + "." + this.id);
-		System.out.println(this.name);
-	};
-	
+	abstract public getPrice();
+
 }
