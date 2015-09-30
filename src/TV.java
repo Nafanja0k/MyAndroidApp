ae7c61a7ba7b2a05c1390f88ba865c07b61b29c2
Nafanja0k <sanja.nafanja@gmail.com>
2015-09-30 09:42:30 +0300

    Update TV.java

--- src/TV.java
+++ src/TV.java
@@ -1,8 +1,16 @@
+public class Tv extends Technic {
 
-public class TV extends Technics {
-
-	public TV() {
+	// націнка у зв’язку з усушкою/утряскою фруктів)
+	protected double storageFee = 1,01;
+	// номер групи/класу. Використовується для генерації номера товару
+	protected classId=1;
+	
+	public Tv() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public double getPrice(){
+		return this.inPrice * this.margin * this.storageFee;
 	}
 
 }
