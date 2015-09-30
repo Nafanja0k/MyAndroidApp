1f518d1a78bb3bcd6bde07bbe145e9205d7f63a0
Nafanja0k <sanja.nafanja@gmail.com>
2015-09-30 09:41:37 +0300

    Update Technics.java

--- src/Technics.java
+++ src/Technics.java
@@ -1,8 +1,15 @@
 
-public class Technics {
-	private String groupName = "Technic";
-	public Technics() {
+public class Technic extends Product{
+	// Торгова націнка на їжу
+	protected margin = 1,1;
+	// номер групи/класу. Використовується для генерації номера товару
+	protected classId=2;
+	
+	public Technic() {
+		
 		// TODO Auto-generated constructor stub
+	
 	}
-
+	
+	abstract public getPrice();
 }
