e5b67c1f765481ff1e36da09ce688f494fd4fa74
Nafanja0k <sanja.nafanja@gmail.com>
2015-10-01 17:46:13 +0300

    added print of items

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 public class Main
 {
 	public static void main(String[] args)
@@ -12,35 +13,16 @@ public class Main
 		// Заповнюємо список продуктів
 		FillProducts.fill(products);
 		
-		ConsoleMenu.mainMenu();
-		/*
-		String inString;
-		Scanner sc = new Scanner(System.in);
-		inString = sc.next();
-		System.out.println(inString);
+		ConsoleMenu.mainMenu(products);
 		
-		
-			switch (inString.toLowerCase())
-			{
-				case "1": {
-					ConsoleMenu.subProductsMenu();
-					break;
-					}
-				case "2": {
-					ConsoleMenu.subTechnicsMenu();
-					break;
-					}
-				case "3": {
-					ConsoleMenu.cartMenu();
-					break;
-					}
-			}
-		*/
-		for (Product item : products) {
+			
+		 
+		/*for (Product item : products) {
 			System.out.println(item.getId() + " - " + item.getName());
 			System.out.println("Вх. ціна: " + item.getInPrice() + " націнка :" + item.getMargin() + " роздрібна ціна: " + item.getPrice());
 			System.out.println("--------");
 		}
+		*/
 		//myCart.addToCart();
 
 
